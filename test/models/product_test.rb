7fe542dfb497b2ae1e3f0a37a3ac6f60fa8f15da
jwg18 <jgraham@taphere.com>
2019-01-29 10:55:04 -0500

    Validation!

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -51,7 +51,8 @@ class ProductTest < ActiveSupport::TestCase
                           price: 1,
                           image_url: "fred.gif")
     assert product.invalid?
-    assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
+    assert_equal ["has already been taken"], product.errors[:title]
+      # assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
   end
 
 end
