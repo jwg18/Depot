8f28432b6484d4a07bb1c525eccf59c8509e2087
jwg18 <jgraham@taphere.com>
2019-04-09 15:04:03 -0400

    Updated Cart & Made it smarter

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,8 +1,13 @@
 class StoreController < ApplicationController
-  def index
-    @products = Product.order(:title) # The :title orders the products by alphabetical order
+  def page_counter
     if session[:counter].nil?
       session[:counter] = 0
     end
+    session[:counter] += 1
+  end
+  def index
+    @products = Product.order(:title) # The :title orders the products by alphabetical order
+    @count = page_counter
+    @show_message = "You've been on this page #{@count} times" if @count > 5
   end
 end
